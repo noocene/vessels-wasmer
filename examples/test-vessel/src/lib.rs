0bbda657f94f26bcde6a2ed8e1d2b9e4205aa385
Izzy Swart <zenerboson@gmail.com>
2020-06-06 20:20:51 -0700

    use executor from main crate

--- examples/test-vessel/src/lib.rs
+++ examples/test-vessel/src/lib.rs
@@ -1,14 +1,13 @@
 use core_futures_io::copy;
+use futures::task::SpawnExt;
 use vessels::{export, VesselEntry};
-pub mod executor;
-use executor::spawn;
 
 export! {
-    VesselEntry { mut reader, mut writer } => {
-        spawn(async move {
+    VesselEntry { spawner, mut reader, mut writer } => {
+        spawner.spawn(async move {
             copy(&mut reader, &mut writer)
                 .await
                 .unwrap_or_else(|_| panic!());
-        })
+        }).unwrap();
     }
 }
